c6afbfe610a366e32921d0bac4e8ca894e22dda1
Peter Felis <Peter@felis.nl>
2022-08-23 14:13:51 +0200

    aanpassing

--- src/routes/component/+page.svelte
+++ src/routes/component/+page.svelte
@@ -1,10 +1,9 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
-	import Shol from '$lib/components/Shol.svelte';
-	export let data;
-	let pag = data.pagina;
 </script>
 
 <svelte:head>
 	<title>bier</title>
 </svelte:head>
+
+Dit is de component pagina
